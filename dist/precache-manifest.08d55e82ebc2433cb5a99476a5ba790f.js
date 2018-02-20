1f4f8919e03834ae73cdbd0e1d89dee4ca3d1b7f
Артём Каширин <kshart@yandex.ru>
2018-02-20 22:27:24 +0500

    .

---
+++ dist/precache-manifest.08d55e82ebc2433cb5a99476a5ba790f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "94956e35b82cbf6dc883",
+    "url": "/js/vendor.94956e35.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "e542f2be27614a21c825",
+    "url": "/js/app.e542f2be.js"
+  },
+  {
+    "revision": "5c3f358a537e266750d7bf428c6b5076",
+    "url": "/index.html"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "e542f2be27614a21c825",
+    "url": "/css/app.0065236b.css"
+  }
+];
